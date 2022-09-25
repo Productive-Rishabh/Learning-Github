c628e9a8820dc117d21a08ef91405c8553383d69
Rishabh Mehta <Rishabh15mehta@gmail.com>
2022-09-25 13:05:07 +0530

    Python calculator

---
+++ file.py
@@ -0,0 +1,13 @@
+x = int(input ("Enter value of x: "))
+y = int(input ("Enter value of y: "))
+print("Sum: ",x+y)
+
+print ("Subtraction: ",x-y)
+
+print ("Multiplication: ",x*y)
+
+print ("Division: ",x/y)
+
+print ("Floor Divide:", x//y)
+
+print ("Exponent",x**y)
